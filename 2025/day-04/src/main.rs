837b8a834388368bf819c689b13b320f52938de0
thomas-gons <79227966+thomas-gons@users.noreply.github.com>
2025-12-10 01:13:35 +0100

    add: day-4 first part

---
+++ 2025/day-04/src/main.rs
@@ -0,0 +1,64 @@
+use std::fs;
+
+const DATA_PATH: &str = "data/rolls-layout-input.txt";
+
+fn read_data() -> Result<String, std::io::Error> {
+    return fs::read_to_string(DATA_PATH)
+}
+
+fn part_one(input: &str) -> usize {
+    let rolls_layout: Vec<Vec<bool>> = input
+        .lines()
+        .map(|line| line.chars().map(|c| c == '@').collect())
+        .collect();
+
+    let height = rolls_layout.len();
+    let width = rolls_layout[0].len();
+    let directions = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    let mut accessible_count = 0;
+
+    for y in 0..height {
+        for x in 0..width {
+            if !rolls_layout[y][x] {
+                continue;
+            }
+
+            let neighbor_count = directions.iter().filter(|&(dy, dx)| {
+                let (nx, ny) = (dx + (x as isize), dy + (y as isize));
+                if nx < 0 || nx >= width as isize || ny < 0 || ny >= height as isize {
+                    return false;
+                }
+                rolls_layout[ny as usize][nx as usize] == true
+            }).count();
+
+            if neighbor_count < 4 {
+                accessible_count += 1;
+            }
+        }
+    }
+
+    accessible_count
+}
+
+
+// fn part_two(input: &str) -> usize {
+//     0
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = read_data()?;
+    println!("The solution for the first part is {}", part_one(&input));
+    // println!("The solution for the second part is {}", part_two(&input));    
+    
+    Ok(())
+}
